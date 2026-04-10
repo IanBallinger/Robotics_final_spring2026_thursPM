8f4a7cb5b44a3efc701d5eb956a58bbb791dcd4f
2.12/2.120 Jetson <karanmah@mit.edu>
2026-04-09 20:09:37 -0400

    testing serial connection from jetson to esp32

---
+++ mobile_robot/test/test_serial_connection.py
@@ -0,0 +1,13 @@
+import serial
+
+def test_serial_con():
+    ser = serial.Serial(port='/dev/ttyTHS3', baudrate=9600, timeout=1)
+    if ser.is_open:
+        print(f"connected to {ser.port}")
+
+    line = ser.readline().decode('utf-8').rstrip()
+    print(line)
+
+
+if __name__=="__main__":
+    test_serial_con()
