5a19dca7c8913d91f521b4fce2267e2669508185
Karan <karanmah@mit.edu>
2026-04-30 23:40:35 -0400

    comment out zero command

--- mobile_robot/missions/run_mission.py
+++ mobile_robot/missions/run_mission.py
@@ -874,7 +874,7 @@ class MissionRuntime:
                 if path_blocked:
                     cmd = self._zero_drive_command()
                 
-                cmd = self._zero_drive_command()
+                # cmd = self._zero_drive_command()
                 self.blackboard.set("obstacle_blocking_path", path_blocked)
                 self.serial.send_wheel_cmd(*cmd.wheel_rates)
                 self.serial.flush_tx()
