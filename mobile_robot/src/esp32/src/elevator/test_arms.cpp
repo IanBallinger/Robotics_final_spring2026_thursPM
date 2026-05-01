607d0d1aa09600eb25cf7132400093a37cdafaff
Karan <karanmah@mit.edu>
2026-05-01 14:47:38 -0400

    fixed pins

--- mobile_robot/src/esp32/src/elevator/test_arms.cpp
+++ mobile_robot/src/esp32/src/elevator/test_arms.cpp
@@ -1,7 +1,9 @@
 #include <ESP32Servo.h>
 
-constexpr int ARM_SHOULDER_SERVO_PIN = 7;
-constexpr int ARM_ELBOW_SERVO_PIN = 6;
+// P1 41, D2 40, P2 42
+
+constexpr int ARM_SHOULDER_SERVO_PIN = 40;
+constexpr int ARM_ELBOW_SERVO_PIN = 41;
 constexpr int SERVO_MIN_US = 544;
 constexpr int SERVO_MAX_US = 2400;
 
@@ -17,11 +19,14 @@ void setup(){
 void loop() {
   // Move to center position
   shoulder_servo.write(90); 
+  Serial.println("90");
   delay(1000);
   // Move to 0 degrees
   shoulder_servo.write(0); 
+  Serial.println("0");
   delay(1000);
   // Move to 180 degrees
   shoulder_servo.write(180);
+  Serial.println("180");
   delay(1000);
 }
