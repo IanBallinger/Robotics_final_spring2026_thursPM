052e45f48936e6147beb320d4c87149d0191ca97
Karan <karanmah@mit.edu>
2026-05-01 22:51:44 -0400

    update pins

--- mobile_robot/src/esp32/src/elevator/test_arms.cpp
+++ mobile_robot/src/esp32/src/elevator/test_arms.cpp
@@ -2,7 +2,7 @@
 
 // P1 7, D2 3, P2 6
 
-constexpr int ARM_SHOULDER_SERVO_PIN = 3;
+constexpr int ARM_SHOULDER_SERVO_PIN = 0;
 constexpr int ARM_ELBOW_SERVO_PIN = 41;
 constexpr int SERVO_MIN_US = 544;
 constexpr int SERVO_MAX_US = 2400;
@@ -12,21 +12,21 @@ Servo elbow_servo;
 
 void setup(){
     Serial.begin(115200);
-    shoulder_servo.attach(ARM_SHOULDER_SERVO_PIN, SERVO_MIN_US, SERVO_MAX_US);
-    // elbow_servo.attach(ARM_ELBOW_SERVO_PIN, SERVO_MIN_US, SERVO_MAX_US);
+    // shoulder_servo.attach(ARM_SHOULDER_SERVO_PIN, SERVO_MIN_US, SERVO_MAX_US);
+    elbow_servo.attach(ARM_ELBOW_SERVO_PIN, SERVO_MIN_US, SERVO_MAX_US);
 }
 
 void loop() {
   // Move to center position
-  shoulder_servo.write(90); 
+  elbow_servo.write(90); 
   Serial.println("90");
   delay(1000);
   // Move to 0 degrees
-  shoulder_servo.write(0); 
+  elbow_servo.write(0); 
   Serial.println("0");
   delay(1000);
   // Move to 180 degrees
-  shoulder_servo.write(180);
+  elbow_servo.write(180);
   Serial.println("180");
   delay(1000);
 }
