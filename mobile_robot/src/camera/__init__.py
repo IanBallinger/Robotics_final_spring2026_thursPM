c434c65f0588fe1160a52b1ae2f8e3d78dca488e
Karan <karanmah@mit.edu>
2026-05-02 23:19:19 -0400

    init

--- mobile_robot/src/camera/__init__.py
+++ mobile_robot/src/camera/__init__.py
@@ -22,7 +22,17 @@ from .transforms import (
     resize_image,
     undistort_image,
 )
+from .tray_detection import HSVBounds, TrayDetectionResult, hsv_mask, isolate_tray_and_find_centroid
 from .types import CameraExtrinsics, CameraFrame, CameraIntrinsics, DepthFrame, StreamConfig
+from .utils import (
+    camera_to_arm_point,
+    camera_to_robot_point,
+    default_camera_config_path,
+    load_camera_to_arm_extrinsics,
+    load_camera_to_robot_extrinsics,
+    robot_point_to_world,
+    rotation_matrix_from_rpy,
+)
 
 __all__ = [
     "apply_extrinsics",
@@ -35,10 +45,21 @@ __all__ = [
     "depth_pixel_to_world_point",
     "depth_to_meters",
     "depth_pixel_to_camera_point",
+    "HSVBounds",
+    "hsv_mask",
+    "isolate_tray_and_find_centroid",
+    "camera_to_arm_point",
+    "camera_to_robot_point",
+    "default_camera_config_path",
+    "load_camera_to_arm_extrinsics",
+    "load_camera_to_robot_extrinsics",
     "OpenCVCamera",
     "pixels_to_world_on_plane",
     "RealSenseCamera",
     "resize_image",
+    "robot_point_to_world",
+    "rotation_matrix_from_rpy",
     "StreamConfig",
+    "TrayDetectionResult",
     "undistort_image",
 ]
