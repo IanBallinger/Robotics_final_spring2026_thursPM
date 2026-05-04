59a557c04362cfdd20be7dc6b93d3d9eb6149eaf
Karan <karanmah@mit.edu>
2026-05-04 15:13:48 -0400

    get serial

--- mobile_robot/test/test_realsense_connect.py
+++ mobile_robot/test/test_realsense_connect.py
@@ -1,16 +1,17 @@
-import pyrealsense2 as rs    
+import pyrealsense2 as rs
 
 ctx = rs.context()
-devices = ctx.query_devices()
-for dev in devices:
-    dev.hardware_reset()
-                                                                                                                                                    
-pipeline = rs.pipeline()                                                                                                                             
-config = rs.config()                                                                                                                                 
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)                                                                                  
-                                                                                                                                                    
-profile = pipeline.start(config)                                                                                                                     
-print("started ok")                                                                                                                                  
-frames = pipeline.wait_for_frames()                                                                                                                  
-print("got frames")                                                                                                                                  
+for dev in ctx.query_devices():
+    name = dev.get_info(rs.camera_info.name)
+    serial = dev.get_info(rs.camera_info.serial_number)
+    print(name, serial)
+
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)
+
+profile = pipeline.start(config)
+print("started ok")
+frames = pipeline.wait_for_frames()
+print("got frames")
 pipeline.stop()
